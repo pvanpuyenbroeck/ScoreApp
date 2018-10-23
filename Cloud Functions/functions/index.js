46396d16a8fcf084e25bde145f72a4663109f612
Pieter Van Puyenbroeck <pieter@Pieters-MBP.lan>
2018-10-23 22:06:32 +0200

    Cloud function to resize working

--- Cloud Functions/functions/index.js
+++ Cloud Functions/functions/index.js
@@ -1,33 +1,47 @@
 const functions = require('firebase-functions');
-const gcs = require('@google-cloud/storage')();
-const os = require('os');
+const {Storage} = require('@google-cloud/storage');
+const spawn = require('child-process-promise').spawn;
 const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
+// const gcs = new Storage({
+//     projectId: 'gs://score-app-b69dc.appspot.com',
+//     keyFilename: '/path/to/keyfile.json'
+//   });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.onFileChange = functions.storage.object().onFinalize((event) => {
-    const object = event.data;
+exports.onFileChange= functions.storage.object().onFinalize(event => {
+    console.log(event);
+    const gcs = admin.storage();
+    const object = event;
     const bucket = object.bucket;
     const contentType = object.contentType;
-    const filePath = object.namecd;
-    console.log("file change detected, function execution started");
+    const filePath = object.name;
+    console.log('File change detected, function execution started');
 
-    if(path.basename(filePath).startsWith('renamed-')){
-        console.log('we already renamed that file');
+    if (object.resourceState === 'not_exists') {
+        console.log('We deleted a file, exit...');
         return;
     }
 
+    if (path.basename(filePath).startsWith('resized-')) {
+        console.log('We already renamed that file!');
+        return;
+    }
     const destBucket = gcs.bucket(bucket);
     const tmpFilePath = path.join(os.tmpdir(), path.basename(filePath));
-    const metadata = { content: contentType }
+    console.log(metadata);
+    const metadata = { contentType: contentType };
     return destBucket.file(filePath).download({
-        destination:tmpFilePath,
+        destination: tmpFilePath
+    }).then(() => {
+        return spawn('convert', [tmpFilePath, '-resize', '500x500', tmpFilePath]);
     }).then(() => {
         return destBucket.upload(tmpFilePath, {
-            destination: 'renamed-' + path.basename(filePath),
-            metadata: metadata,
+            destination: 'resized-' + path.basename(filePath),
+            metadata: metadata
         })
-    })
-})
+    });
+});

1f3e453b8544358d56c7a634d5463908537469c4
Pieter Van Puyenbroeck <pieter@Pieters-MBP.lan>
2019-02-27 14:00:13 +0100

    Adding and removing with confirm alert working

--- src/components/Alerts/Confirm/confirm.js
+++ src/components/Alerts/Confirm/confirm.js
@@ -3,16 +3,24 @@ import classes from './confirm.css';
 
 export const confirm = props => {
 
-    return(
-        <div className={classes.ConfirmContainer}>
+    const style = {
+        display: `${props.showConfirm ? 'inline-block' : 'none'}`
+    }
+
+    return (
+        <div className={classes.ConfirmContainer} style={style}>
             <div className={classes.Message}>
                 {props.message}
             </div>
-            <div className={[classes.Button1, classes.Button].join(' ')}>
-            {props.label1}
-            </div>
-            <div className={[classes.Button2, classes.Button].join(' ')}>
-            {props.label2}
+            <div className={classes.ButtonContainer}>
+                <div className={[classes.Button1, classes.Button].join(' ')}
+                onClick={props.label1Clicked}>
+                    <h3>{props.label1}</h3>
+                </div>
+                <div className={[classes.Button2, classes.Button].join(' ')}
+                onClick={props.label2Clicked}>
+                    <h3>{props.label2}</h3>
+                </div>
             </div>
         </div>
     )
--- src/containers/TeamControl/Team/Team.js
+++ src/containers/TeamControl/Team/Team.js
@@ -9,6 +9,7 @@ import MatchCenter from '../MatchCenter/MatchCenter';
 import * as actions from '../../../store/actions/index';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Aux from '../../../hoc/_Aux/_Aux';
+import Confirm from '../../../components/Alerts/Confirm/confirm';
 
 class Team extends Component {
 
@@ -36,8 +37,10 @@ class Team extends Component {
         showModal: false,
         showToggle: false,
         showMatchControl: false,
-        selectedMatch:{},
-        teamMembers:{},
+        selectedMatch: {},
+        teamMembers: {},
+        showConfirm: false,
+        playeridToRemove: "",
     }
     // pick(obj, keys) {
     //     return keys.map(k => k in obj ? { [k]: obj[k] } : {})
@@ -69,43 +72,59 @@ class Team extends Component {
 
     removePlayerClickedHandler(playerId) {
         // alert('Wil je deze speler verwijderen uit uw team?');    
-        this.props.removePlayerFromTeam(playerId, this.props.team.teamId, this.props.team.TeamMembers)
-        this.props.selectedTeam(this.props.team.teamId, this.props.token, this.props.userId);
+        this.setState({ showConfirm: true, playeridToRemove:playerId });
     }
+
+    confirmHandler(value) {
+        if (value) {
+            this.props.removePlayerFromTeam(this.state.playeridToRemove, this.props.team.teamId, this.props.team.TeamMembers)
+            this.props.selectedTeam(this.props.team.teamId, this.props.token, this.props.userId);
+        }
+        this.setState({showConfirm:false, playeridToRemove:""});
+    }
+
     render() {
         let teamControl = "";
-        if(this.props.loading){
-            teamControl = <Spinner/>;
+        if (this.props.loading) {
+            teamControl = <Spinner />;
         }
-        else{
-        if (this.state.showMatchControl) {
-            teamControl = <MatchCenter />
-        } else {
-            teamControl = (
-                <Aux>
-                    {/*}<Toggle toggleClicked={() => this.props.showFunctionMenu()} classtheme="TeamButton">MY TEAM</Toggle>*/}
-                    <TeamFunctionMenu 
-                    team={this.props.team} 
-                    showFunctionMenu={this.props.showfunctionMenu} 
-                    showComponent={(component) => this.props.showComponent(component)}
-                    />
-                    <Modal show={this.props.showModal} modalClosed={() => this.props.closeModal()} />
-                    <Players 
-                    team={this.props.team} 
-                    playerDetails={this.props.team.filteredPlayers} 
-                    user={this.props.user}
-                    removePlayerClicked={(uid) =>this.removePlayerClickedHandler(uid)}
-                    />
-                    <Games 
-                    matches={this.props.team.Matches} 
-                    teamId={this.props.team.teamId} 
-                    matchClicked={(match) => this.matchSelected(match)} 
-                    team={this.props.team}                    
-                    />
-                </Aux>
-            )
+        else {
+            if (this.state.showMatchControl) {
+                teamControl = <MatchCenter />
+            } else {
+                teamControl = (
+                    <Aux>
+                        {/*}<Toggle toggleClicked={() => this.props.showFunctionMenu()} classtheme="TeamButton">MY TEAM</Toggle>*/}
+                        <Confirm
+                            message={"Weet je zeker dat je deze speler wil verwijderen?"}
+                            label1={"YES"}
+                            label2={"NO"}
+                            label1Clicked={() => this.confirmHandler(true)}
+                            label2Clicked={() => this.confirmHandler(false)}
+                            showConfirm={this.state.showConfirm}
+                        />
+                        <TeamFunctionMenu
+                            team={this.props.team}
+                            showFunctionMenu={this.props.showfunctionMenu}
+                            showComponent={(component) => this.props.showComponent(component)}
+                        />
+                        <Modal show={this.props.showModal} modalClosed={() => this.props.closeModal()} />
+                        <Players
+                            team={this.props.team}
+                            playerDetails={this.props.team.filteredPlayers}
+                            user={this.props.user}
+                            removePlayerClicked={(uid) => this.removePlayerClickedHandler(uid)}
+                        />
+                        <Games
+                            matches={this.props.team.Matches}
+                            teamId={this.props.team.teamId}
+                            matchClicked={(match) => this.matchSelected(match)}
+                            team={this.props.team}
+                        />
+                    </Aux>
+                )
+            }
         }
-    }
         return (
             <Aux>
                 {teamControl}
@@ -123,7 +142,7 @@ const mapDispatchToProps = dispatch => {
         showComponent: (component) => dispatch(actions.showComponent(component)),
         matchSelected: (match) => dispatch(actions.setSelectedMatchInfo(match)),
         changeLocation: (location) => dispatch(actions.locationChange(location)),
-        removePlayerFromTeam: (playerid, teamid, teamMembers) => dispatch(actions.removePlayerFromTeam(playerid,teamid,teamMembers))
+        removePlayerFromTeam: (playerid, teamid, teamMembers) => dispatch(actions.removePlayerFromTeam(playerid, teamid, teamMembers))
     }
 }
 
@@ -135,7 +154,7 @@ const mapStateToProps = state => {
         loading: state.team.loading,
         token: state.auth.token,
         userId: state.auth.userId,
-        user:state.auth.user,
+        user: state.auth.user,
     }
 }
 

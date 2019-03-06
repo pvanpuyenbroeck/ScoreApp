26bb610ef97c4aa81521e03e264673efa7b9713c
Pieter Van Puyenbroeck <pieter@Pieters-MBP.lan>
2019-03-06 21:47:44 +0100

    Match css update

--- src/components/Games/Match/Match.js
+++ src/components/Games/Match/Match.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Match.css';
 import { Link } from 'react-router-dom';
 import { StyledLink } from './MatchStyle';
 import styled from 'styled-components';
 import { colors } from '../../../utils/styles/helpers';
+import CloseButton from '../../../assets/Images/delete-30.png';
 
 
 const Match = (props) => {
+    const [showOptions, setShowOptions] = useState(classes.HideOptions);
+
     let homeGoals = 0;
     for (let key in props.match.Participants) {
         homeGoals += props.match.Participants[key].goals;
@@ -31,20 +34,44 @@ const Match = (props) => {
         return `${day}-${month}-${year}  ${hour}:${minutes}`
     }
 
+    const setShowHideOptionsHandler = (showClass) => {
+        setShowOptions(showClass);
+    }
+
+    const closeButtonStyle = {
+        backgroundImage: `url(${CloseButton})`,
+        // visibility: `${this.state.closeButton}`
+    }
+
+    // const showHideOptionsClasses = [classes.Match,showOptions]
+
     return (
-        <Link to={"/Team/" + props.team.teamId + "/Match/" + props.match.matchId} style={{ textDecoration: 'none' }}>
-            <StyledLink className={classes.Match} onClick={() => props.matchButtonClicked(props.match)}>
-                {/* <div>Afbeelding komt hier</div> */}
-                <div>
-                    <TeamName>{props.team.teamName}</TeamName>
-                    <OponentName>
-                        <div>{typeof props.match.gameData.opponent === 'undefined' ? null : props.match.gameData.opponent}</div>
-                    </OponentName>
-                    <div className={classes.Score}>{homeGoals} - {props.match.oponentGoals}</div>
-                    <div className={classes.GameDate}>{dateFormat()}</div>
-                </div>
-            </StyledLink>
-        </Link>
+        <React.Fragment>
+            <div className={showOptions}>
+                <div
+                    style={closeButtonStyle} 
+                    className={classes.CloseButton} 
+                ></div>
+            </div>
+            <Link to={"/Team/" + props.team.teamId + "/Match/" + props.match.matchId} style={{ textDecoration: 'none' }}>
+                <StyledLink
+                    className={classes.Match}
+                    onClick={() => props.matchButtonClicked(props.match)}
+                    onMouseEnter={() => setShowHideOptionsHandler(classes.ShowOptions)}
+                    onMouseLeave={() => setShowHideOptionsHandler(classes.HideOptions)}
+                >
+                    {/* <div>Afbeelding komt hier</div> */}
+                    <div>
+                        <TeamName>{props.team.teamName}</TeamName>
+                        <OponentName>
+                            <div>{typeof props.match.gameData.opponent === 'undefined' ? null : props.match.gameData.opponent}</div>
+                        </OponentName>
+                        <div className={classes.Score}>{homeGoals} - {props.match.oponentGoals}</div>
+                        <div className={classes.GameDate}>{dateFormat()}</div>
+                    </div>
+                </StyledLink>
+            </Link>
+        </React.Fragment>
     )
 }
 

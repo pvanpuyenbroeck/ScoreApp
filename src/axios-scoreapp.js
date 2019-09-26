888e1b0f7d58367267ff651535ce4cb8a757abbb
Pieter Van Puyenbroeck <pieter@Pieters-MBP.lan>
2019-09-26 21:16:20 +0200

    Build

--- src/axios-scoreapp.js
+++ src/axios-scoreapp.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instanceTest = axios.create({
-    baseURL:'https://score-app-b69dc.firebaseio.com/'
+    baseURL: 'https://score-app-b69dc.firebaseio.com/'
 });
 
 // const instanceLive = axios.create({
